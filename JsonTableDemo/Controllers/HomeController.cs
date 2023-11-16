ff9107f5dc3de576426d77a333bcfaf66083a7a5
Waqar Abdul Ghaffar <waqar@tplex.com>
2023-11-16 21:57:27 +0400

    initial

---
+++ JsonTableDemo/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using JsonTableDemo.Models;
+using JsonTableDemo.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace JsonTableDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly DataService _dataService;
+
+        public HomeController(ILogger<HomeController> logger, DataService dataService)
+        {
+            _logger = logger;
+            _dataService = dataService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var people = await _dataService.GetPeopleAsync();
+            return View(people);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ JsonTableDemo/Services/DataService.cs
@@ -0,0 +1,23 @@
+﻿using JsonTableDemo.Models;
+using Newtonsoft.Json;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+
+namespace JsonTableDemo.Services;
+
+public class DataService
+{
+    private readonly IWebHostEnvironment _env;
+
+    public DataService(IWebHostEnvironment env)
+    {
+        _env = env;
+    }
+
+    public async Task<List<Person>> GetPeopleAsync()
+    {
+        var filePath = Path.Combine(_env.WebRootPath, "data.json");
+        var json = await System.IO.File.ReadAllTextAsync(filePath);
+        var people = JsonSerializer.Deserialize<List<Person>>(json);
+        return people;
+    }
+}
